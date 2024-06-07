2b3b5eccd1c6c3c5a5e5bca398fcc0bc881bbfa8
Aleyna Kaya <devaleykaya@gmail.com>
2024-06-08 02:09:57 +0300

    üçgen alan hesaplama

---
+++ 13-ucgen_alan_hesaplama.py
@@ -0,0 +1,14 @@
+"""
+    Üçgenin alanını hesapla
+"""
+
+
+# Üçgen alanı formülü >> Üçgen: alan = (taban * yükseklik) / 2
+
+
+taban = int(input("Üçgenin taban uzunluğunu giriniz: "))
+yukseklik = int(input("Üçgenin yüksekliğini giriniz: "))
+
+alan = (taban * yukseklik) / 2
+
+print("Üçgenin alanı: ",alan)
