67f3e3a4d5d992af253951ffa149ea66a6dd2a48
Aleyna Kaya <devaleykaya@gmail.com>
2024-06-07 11:42:21 +0300

    kare alanı hesaplama

---
+++ 08-kare_alan_hesaplama.py
@@ -0,0 +1,10 @@
+"""
+    Karenin alanını hesapla
+"""
+
+
+# Karenin alanı >> Kare: alan = kenar ** 2
+
+kenar = int(input("Karenin bir kenar uzunluğunu giriniz: "))
+alan = kenar ** 2
+print(alan)
