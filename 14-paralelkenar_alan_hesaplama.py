28ea3090de507c18f27408a1a1eaeb6539fe38a4
Aleyna Kaya <devaleykaya@gmail.com>
2024-06-08 02:10:59 +0300

    paralelkenar alanı hesaplama

---
+++ 14-paralelkenar_alan_hesaplama.py
@@ -0,0 +1,13 @@
+"""
+    Paralelkenar alanını hesapla
+"""
+
+
+# Paralelkenar alanı >> Paralelkenar: alan = taban * yükseklik
+
+taban = int(input("Paralelkenarın taban uzunluğunu giriniz: "))
+yukseklik = int(input("Paralelkenarın yüksekliğini giriniz: "))
+
+alan = taban * yukseklik
+
+print(f"Paralelkenarın alanı: {alan}")
