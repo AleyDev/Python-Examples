3b28db117a0e04c8ffbd075a1f1fb53c8cfd6cd6
Aleyna Kaya <devaleykaya@gmail.com>
2024-06-07 11:45:30 +0300

    mutlak deger hesaplama

---
+++ 10-mutlak_deger.py
@@ -0,0 +1,12 @@
+"""
+    Girilen bir sayının mutlak değerini hesaplama
+"""
+
+
+sayi = int(input("Sayı: "))  # Kullanıcıdan bir sayı girişi al
+
+#Sayının mutlak değerini hesapla
+mutlak_deger = abs(sayi)
+
+# Hesaplanan mutlak değeri ekrana yazdır
+print("Sayının mutlak değeri:", mutlak_deger)
